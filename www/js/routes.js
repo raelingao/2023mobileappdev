56d95d1994a9ab3efcae24c3f1f257239d40dc72
Helen Zhang <hajzhang@gmail.com>
2023-01-09 22:07:27 -0500

    Initial commit

---
+++ www/js/routes.js
@@ -0,0 +1,15 @@
+var routes = [
+  {
+    "path": "/",
+    "url": "./index.html",
+    "name": "index"
+  },
+  {
+    "path": "/calendar",
+    "url": "./calendar.html"
+  },
+  {
+    "path": "/send-here",
+    "url": "./send-here.html"
+  }
+]
