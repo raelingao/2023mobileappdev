050c2eb4355a29d2aec6310c7383b4ffb2a054ba
helemzh <hajzhang@gmail.com>
2023-01-16 14:29:52 -0500

    pages for navbar

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,15 +1,42 @@
 var routes = [
   {
-    "path": "/",
+    "path": "/indexPage",
+    "pageName": "indexPage"
+  },
+  {
+    "path": "/attendance",
+    "url": "./attendance.html"
+  },
+  {
+    "path": "/index",
+    "name": "index",
     "url": "./index.html",
-    "name": "index"
+    "master": true
   },
   {
     "path": "/calendar",
-    "url": "./calendar.html"
+    "url": "calendar.html"
   },
   {
     "path": "/send-here",
     "url": "./send-here.html"
+  },
+  {
+    "path": "/lunchmenu",
+    "url": "lunchmenu.html"
+  },
+  {
+    "path": "/login",
+    "url": "./login.html"
+  },
+  {
+    "path": "/social",
+    "name": "social",
+    "url": "./social.html"
+  },
+  {
+    "path": "/menu",
+    "url": "./menu.html",
+    "name": "menu"
   }
 ]
